993cefd122075adb61b33898acb25746e73b5bc2
juan-m-m <juan-es20@hotmail.com>
2019-10-23 21:13:18 -0400

    first commit

---
+++ src/main/java/com/jmontano/automationfespring/AutomationFeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jmontano.automationfespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationFeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationFeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmontano/automationfespring/AutomationFeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmontano.automationfespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationFeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
